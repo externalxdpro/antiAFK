ec8c449053f53c245ba957f77e8f31b1370e0866
externalxdpro <externalxdpro@gmail.com>
2022-10-04 12:39:30 -0400

    Add basic random letter entering

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include <windows.h>
 
 using namespace std;
+using namespace this_thread;
+using namespace chrono;
 
 int main()
 {
     while (true)
     {
-        keybd_event(0x45, 0, 0, 0);
-        Sleep(1000);
+        int minVal = 0x41;
+        int maxVal = 0x5A;
+
+        for (int i = 0; i < 26; i++)
+        {
+            srand(time(nullptr));
+            int randKey = rand() % (maxVal - minVal + 1) + minVal;
+            keybd_event(randKey, 0, 0, 0);
+            sleep_for(seconds(1));
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            keybd_event(VK_BACK, 0, 0, 0);
+            sleep_for(milliseconds(100));
+        }
     }
 
     return 0;
