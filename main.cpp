252730a082ebd486fd6c419e945289c537c9c2d4
externalxdpro <externalxdpro@gmail.com>
2022-10-05 13:37:10 -0400

    Add variable for number of characters to print

--- main.cpp
+++ main.cpp
@@ -13,8 +13,9 @@ int main()
     {
         int minVal = 0x41;
         int maxVal = 0x5A;
+        int chars = 100;
 
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < chars; i++)
         {
             srand(time(nullptr));
             int randKey = rand() % (maxVal - minVal + 1) + minVal;
@@ -22,7 +23,7 @@ int main()
             sleep_for(seconds(1));
         }
 
-        for (int i = 0; i < 26; i++)
+        for (int i = 0; i < chars; i++)
         {
             keybd_event(VK_BACK, 0, 0, 0);
             sleep_for(milliseconds(100));
